86f4b84d53a9e5fd0cea182439d38dc81b8d5a47
Teodor Lagerqvist <xio.git@teodor.se>
2016-11-04 10:24:43 +0100

    aded storage.js to handle indexeddb

---
+++ public/js/storage.js
@@ -0,0 +1,132 @@
+class IDB {
+
+	constructor(name, version, upgradeCallback) {
+		this.name = name;
+		this.version = version;
+		this.upgradeCallback = upgradeCallback;
+	}
+
+	open() {
+		let that = this;
+		return new Promise(function(resolve, reject) {
+			if(that.db) resolve(that.db);
+
+			var openRequest = indexedDB.open(that.name, that.version);
+			openRequest.onupgradeneeded = that.upgradeCallback;
+
+			openRequest.onsuccess = function(e) {
+				that.db = e.target.result;
+				resolve(that.db);
+			}
+
+			openRequest.onerror = function(e) {
+				reject(e);
+			}
+		});
+	}
+
+	save(storeName, data) {
+		return this.open().then(function(db) {
+			return new Promise(function(resolve, reject) {
+
+				var transaction = db.transaction([storeName],"readwrite");
+				var store = transaction.objectStore(storeName);
+
+				var request = store.add(data);
+
+				request.onerror = function(e) {
+					reject(e);
+				}
+
+				request.onsuccess = function(e) {
+					resolve(e.target.result);
+				}
+			});
+		});
+	}
+
+	getAll(storeName, index) {
+		return this.open().then(function(db) {
+			return new Promise(function(resolve, reject) {
+				var items = [];
+				var transaction = db.transaction(storeName, "readonly");
+				var store = transaction.objectStore(storeName);
+
+				if(index) {
+					let storeIndex = store.index(index);
+					var cursorRequest = storeIndex.openCursor();
+				} else {
+					var cursorRequest = store.openCursor();
+				}
+
+
+				transaction.oncomplete = function(evt) {
+					resolve(items);
+				};
+
+				cursorRequest.onerror = function(error) {
+					reject(error);
+				};
+
+				cursorRequest.onsuccess = function(evt) {
+					var cursor = evt.target.result;
+					if (cursor) {
+						items.push(cursor.value);
+						cursor.continue();
+					}
+				};
+			});
+		});
+	}
+
+	get(storeName, id) {
+		return this.open().then(function(db) {
+			return new Promise(function(resolve, reject) {
+				var transaction = db.transaction(storeName, "readonly");
+				var store = transaction.objectStore(storeName);
+
+				var request = store.get(id);
+				request.onsuccess = function(e) {
+					resolve(e.target.result);
+				};
+				request.onerror = function(err) {
+					reject(err);
+				};
+			});
+		});
+	}
+
+	delete(storeName, id) {
+		return this.open().then(function(db) {
+			return new Promise(function(resolve, reject) {
+				var transaction = db.transaction(storeName, "readwrite");
+				var store = transaction.objectStore(storeName);
+
+				var request = store.delete(id);
+				request.onsuccess = function(e) {
+					resolve(e);
+				};
+				request.onerror = function(err) {
+					reject(err);
+				};
+			});
+		});
+	}
+
+	update(storeName, id, data) {
+		return this.open().then(function(db) {
+			return new Promise(function(resolve, reject) {
+				var transaction = db.transaction(storeName, "readwrite");
+				var store = transaction.objectStore(storeName);
+
+				var request = store.put(data);
+				request.onsuccess = function(e) {
+					resolve(e);
+				};
+				request.onerror = function(err) {
+					reject(err);
+				};
+			});
+		});
+	}
+}
